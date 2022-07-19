be88e08a04c2ae6629dc9ae7fc77fb7108a2dce7
Miguels15 <72955388+Miguels15@users.noreply.github.com>
2022-07-18 22:01:35 -0600

    Delete Guia #1 directory

--- Guia #1/complementarios/Ejercicio1/JavaScript/yardas.js
+++
@@ -1,6 +0,0 @@
-var num1=prompt("Conversion de metros a yardas, por favor digite un numero:");
-const yarda=1.09361;
-var resultado= num1*yarda;
-var resultado2 = resultado.toFixed(2);
-document.write("<h1>Los metros a convertir son: "+num1+"</h1>");
-document.write("<h1>La conversion de metros a yardas es: " + resultado2 + "</h1>");
